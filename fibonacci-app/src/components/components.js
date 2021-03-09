1fea9c5bd96e8e8f631262113f88d19d1a5606ce
durjim <30879741+durjim@users.noreply.github.com>
2021-03-09 08:33:02 -0600

    Updating components' files

--- fibonacci-app/src/components/components.js
+++ fibonacci-app/src/components/components.js
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { GetFibonacciValue } from '../service-caller/service-caller'
 
+// Regular expression for input validation
+const regex = /^-?[\d]{1,}$/
+
 export function FibonacciIndexInput() {
     const [N, setN] = useState(0);
+    const [isValid, validInput] = useState(true);
+
+    // Event handlers
+    const onChangeInput = e => {
+        setN(e.target.value)
+        validInput(regex.test(e.target.value))
+    }
+
     const getFibonacci = e => {
         GetFibonacciValue(N)
         .then(value => alert(value))
@@ -12,8 +23,8 @@ export function FibonacciIndexInput() {
     return(
         <div>
             <label htmlFor="input">Input the index of the Fibonacci value to retrieve:</label>
-            <input id="input" type="text" defaultValue={ N } onChange={ e => setN(e.target.value) }/>
-            <button onClick={ getFibonacci }>Ok</button>
+            <input id="input" type="text" defaultValue={ N } onChange={ onChangeInput }/>
+            <button onClick={ getFibonacci } disabled={ !isValid }>Ok</button>
         </div>
     );
 
